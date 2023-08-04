d7a0b00962a38be72c1e0a8f08bb67ced218e1dd
rojasubbu <133404557+rojasubbu@users.noreply.github.com>
2023-08-04 14:55:58 +0530

    Delete SOCIAL MEDIA WEBSITE SNSCT CBE directory

--- SOCIAL MEDIA WEBSITE SNSCT CBE/demo/src/main/java/com/example/demo/repos/newsrepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repos;
-
-import com.example.demo.entities.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface newsrepo extends JpaRepository<News, Long> {
-}
